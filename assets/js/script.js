d06bb6e4f82e1ebd060b02798f2506e7fa72d299
NIASKA-Desktop <mkman751@yahoo.com>
2021-04-23 02:59:19 +0900

    successfully added image and series to load in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 $.getJSON("https://www.amiiboapi.com/api/amiibo/?name=mario",
     function(data){
         console.log(data);
+
+    
+    let icon ="https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_00000000-00000002.png";
+    let aName = data.amiibo[0].name;
+    let aSeries = data.amiibo[0].amiiboSeries;
+
+    $('.icon').attr("src", icon);
+    $('.name').append(aName);
+    $('.series').append(aSeries);
     }
 );
