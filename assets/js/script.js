d8fba815058b78e9e13e57664618daa2a2515919
NIASKA-Desktop <mkman751@yahoo.com>
2021-04-26 22:59:25 +0900

    working javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,17 @@ let amiiboFunc = {
     },
 
     showAmiibo: function(data) {
-        const {name} = data.amiibo[0];
-        const {character} =data.amiibo[0];
+        const name = data.amiibo[0].name;
         const amiiboSeries = data.amiibo[0].amiiboSeries;
-        console.log(name, character, amiiboSeries);
-        document.querySelector(".name").innerHTML = " Name is " + name;
+        const gSeries = data.amiibo[0].gameSeries;
+        const releaseDate = data.amiibo[0].release.au;
+        const img = data.amiibo[0].image;
+        document.querySelector(".name").innerHTML = "Name is " + name;
+        document.querySelector(".gSeries").innerHTML = "The character's series is from " + gSeries;
+        document.querySelector(".aSeries").innerHTML = "The Amiibo series is from " + amiiboSeries;
+        document.querySelector(".releaseDate").innerHTML = "The release date in AU is " + releaseDate;
+        document.querySelector(".img").src =  img;
+        
+      
     }
 };
